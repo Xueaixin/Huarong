34f8d1ff17ac1cdc1f1598989d4447392998a47a
xueaixin <wx_99f6c288d3eb40e88ccea4aee5c9cfa1@git.code.tencent.com>
2019-06-04 19:17:16 +0800

    pro1.4

--- HuarongRoad/app/src/main/java/com/example/xaxxu/huarongroad/MainActivity.java
+++ HuarongRoad/app/src/main/java/com/example/xaxxu/huarongroad/MainActivity.java
@@ -6,13 +6,17 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private int mSteps = 0;
+    private TextView mShowSteps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mShowSteps = (TextView)findViewById(R.id.steps_num);
     }
 
     public void showIntroduction(View view) {
@@ -25,5 +29,12 @@ public class MainActivity extends AppCompatActivity {
     public void returnHome(View view) {
         Intent intent = new Intent(this, Home.class);
         startActivity(intent);
+    }
+
+    public void changeSteps(View view) {
+        mSteps++;
+        if(mShowSteps != null) {
+            mShowSteps.setText(Integer.toString(mSteps));
+        }
     }
 }
