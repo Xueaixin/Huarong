b7a3f2f0308f0c9161612b2ac15aa8d15308d69b
xueaixin <wx_99f6c288d3eb40e88ccea4aee5c9cfa1@git.code.tencent.com>
2019-06-04 16:20:22 +0800

    add picture

--- HuarongRoad/app/src/main/java/com/example/xaxxu/huarongroad/MainActivity.java
+++ HuarongRoad/app/src/main/java/com/example/xaxxu/huarongroad/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.xaxxu.huarongroad;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showIntroduction(View view) {
+        AlertDialog.Builder introAlert = new AlertDialog.Builder(MainActivity.this);
+        introAlert.setTitle("游戏说明");
+        introAlert.setMessage("帮助曹操逃脱！");
+        introAlert.show();
     }
 }
